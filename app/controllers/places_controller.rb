bbe97482fae2212c6a623f3e48493c28ad532a23
Geoffrey Waterhouse <geoff.s.waterhouse@gmail.com>
2018-03-08 17:29:00 +0000

    Authentication required for editing places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
   def index
     @places = Place.all.page(params[:page]).per_page(3)
@@ -16,6 +16,11 @@ class PlacesController < ApplicationController
 
   def show
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden      
+    end
+
   end
 
   def edit
@@ -24,6 +29,11 @@ class PlacesController < ApplicationController
 
   def update
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+
     @place.update_attributes(place_params)
     redirect_to root_path
   end
